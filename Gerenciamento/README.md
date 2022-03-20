5b06b1ca7695f4e568c26a668f0041a30000007b
Karen <57016155+karenpaiva@users.noreply.github.com>
2022-03-20 08:51:21 -0300

    Delete Gerenciamento directory

--- Gerenciamento/README.md
+++
@@ -1,3 +0,0 @@
-<h2> Desenvolvendo um sistema de gerenciamento de pessoas em API REST com Spring Boot</h2>
-
-Curso do digital inovation one para desenvolver um sistema de gerenciamento de pessoas.
--- Gerenciamento/src/main/java/api/pessoas/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package api.pessoas.repository;
-
-import api.pessoas.entity.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
